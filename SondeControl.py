e687875e670d96b1903673dfd5d2d760c04d8aeb
cunnia3 <andrew64ce@gmail.com>
2015-08-11 12:47:14 -0400

    added reading from data from Sonde

--- SondeControl.py
+++ SondeControl.py
@@ -15,16 +15,70 @@ import socket
 
 # This class handles all of the communication with
 # the sonde sensor
-class SondeController:
-    'Sonde System communication handler'
 
-    def __init__(self):
-        print "dummy initialized Sonde"
+class SondeController:
+    'Sonde communication handler'
+
+    def __init__(self, portIn = '/dev/ttyUSB0'):
+        self.port = portIn
+        self.lock = threading.RLock()
+        
+        # initialize the serial communication
+        self.ser = serial.Serial(
+            port='/dev/ttyUSB0',
+            baudrate=9600,
+            parity=serial.PARITY_NONE,
+            stopbits=serial.STOPBITS_ONE,
+            bytesize=serial.EIGHTBITS,
+            timeout=1)
+       
+
+    def readTime(self):
+        #flush any bad previous input        
+        self.ser.write('\r')
+        self.ser.write('time\r')
+
+        # read three lines of serial output because the
+        # sonde provides output for terminal interfacing
+        time = None 
+        for x in range(0, 3):
+            time = self.ser.readline()
+           
+        return time
+
+
+    def readData(self):
+        # flush any bad previous input
+        self.ser.write('\r')
+        self.ser.write('data\r')
+
+        # read three lines of serial output because the
+        # sonde provides output for terminal interfacing
+        data = None 
+        for x in range(0, 3):
+            data = self.ser.readline()
+           
+        return data
+
 
     def getData(self):
-        return "dummy line from getCSVLine"
+        self.lock.acquire()
+        try:
+            timeStamp = self.readTime()
+            #remove newline
+            timeStamp = timeStamp.rstrip()
+            data = self.readData()
+            data = data.replace(" ",",")
+       
+            #chop off the last comma
+            data = data[:-3]
+
+        finally:
+            self.lock.release()
+        return timeStamp + "," + data + "\n"
 
     def goToDepth(self, desiredDepth):
         print "dummy set depth to: " + desiredDepth
         time.sleep(2)
         print "done setting dummy depth"
+
